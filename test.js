b88a4e8a0348993b20c417afa52e9f105ab533b5
Pratik Sinha <pratik.sinha@cloudkaptan.com>
2024-10-25 15:39:59 +0530

    adding again

--- test.js
+++ test.js
@@ -2,6 +2,9 @@ var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  terminal: false }); rl.on('line', function(line){
+  terminal: false }); 
+  
+  
+  rl.on('line', function(line){
         console.log("Hello, " + line);
 });
