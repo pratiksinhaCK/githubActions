88e9c2a6a33873d5439a9d34f06e674d14f0fc1e
Pratik Sinha <pratik.sinha@cloudkaptan.com>
2024-10-25 17:55:38 +0530

    msg12

---
+++ src/test.js
@@ -0,0 +1,27 @@
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+
+
+
+  terminal: false }); 
+
+  console.log("absc");
+  
+
+
+
+
+  console.log("absc");
+    
+
+
+
+
+
+
+
+  rl.on('line', function(line){
+        console.log("Hello, " + line);
+});
